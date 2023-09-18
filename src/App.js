f6b0c32879b9c4b277f555601f0bfba7edcf8ec9
Shivanshu Shekhar <shivanshushekhar786@gmail.com>
2023-09-18 19:31:56 +0530

    Initialize Styling

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import React from 'react';
 
 import { Footer, Blog, Possiblity, Features, WhatGPT3, Header } from './containers';
 import { Navbar, CTA, Brand } from './components';
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-        <h1>GPT-3</h1>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possiblity />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   )
 }
