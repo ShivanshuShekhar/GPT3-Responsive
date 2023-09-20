d3f18b413ff87e261dffed9977bd631a0835fdc6
Shivanshu Shekhar <shivanshushekhar786@gmail.com>
2023-09-20 21:51:44 +0530

    Added Possibility Image

--- src/containers/possibility/Possiblity.jsx
+++ src/containers/possibility/Possiblity.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './possibility.css';
 
+import possibilityImage from '../../assets/possibility.png';
+
 const Possiblity = () => {
   return (
-    <div>Possiblity</div>
+    <div className='gpt3__possibility section__padding'>
+      <div className='gpt3__possibility-image'>
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+    </div>
   )
 }
 
